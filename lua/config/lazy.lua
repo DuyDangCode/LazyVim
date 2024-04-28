bad27c84e0a2403a505ff4cdcfb73ca33e958dd2
Thành Duy <21520774@gm.uit.edu.vn>
2024-04-28 21:29:56 +0700

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,57 +8,55 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  
-  spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins",
-      opts = {
-				colorscheme = "terafox",
-			},
 
+    spec = {
+        -- add LazyVim and import its plugins
+        { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+            colorscheme = "terafox",
+        } },
+        -- {
+        --   "lalitmee/cobalt2.nvim",
+        --   dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
+        --   init = function()
+        --       require("colorbuddy").colorscheme("cobalt2")
+        --   end,
+        -- },
+
+        --  in your lua config
+        -- import any extras modules here
+        -- { import = "lazyvim.plugins.extras.lang.typescript" },
+        -- { import = "lazyvim.plugins.extras.lang.json" },
+        -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+        -- import/override with your plugins
+        { import = "plugins" },
     },
-    -- {
-    --   "lalitmee/cobalt2.nvim",
-    --   dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
-    --   init = function()
-    --       require("colorbuddy").colorscheme("cobalt2")
-    --   end,
-    -- },
-   
---  in your lua config
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
-  
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  -- install = { colorscheme = { "terafox"} },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-   -- cache = { enabled = true }, -- cache the plugins
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+
+    defaults = {
+        -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+        lazy = false,
+        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+        -- have outdated releases, which may break your Neovim install.
+        version = false, -- always use the latest git commit
+        -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
-  },
+    -- install = { colorscheme = { "terafox"} },
+    checker = { enabled = true }, -- automatically check for plugin updates
+    performance = {
+        -- cache = { enabled = true }, -- cache the plugins
+        rtp = {
+            -- disable some rtp plugins
+            disabled_plugins = {
+                "gzip",
+                -- "matchit",
+                -- "matchparen",
+                -- "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+                "copilot",
+            },
+        },
+    },
 })
